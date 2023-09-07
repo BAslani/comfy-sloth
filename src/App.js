2c12964d996c04045c77a1105af897787b79d181
Behzad Aslani <behzadaslani80@gmail.com>
2023-09-07 15:32:42 +0330

    build: boilerplates
    creating boilerplates for pages and importing them in the App component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home, About, Products, SingleProduct, Cart, Error, Checkout, PrivateRoute
+} from './pages'
+import styled from 'styled-components'
+
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/:id' element={<SingleProduct />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
