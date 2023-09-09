f14db2131c132a45b931fce9460bd68fbb9413c5
Behzad Aslani <behzadaslani80@gmail.com>
2023-09-09 16:51:17 +0330

    build: Single Product Page setup

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -10,6 +10,7 @@ import {
 } from '../actions'
 
 const products_reducer = (state, action) => {
+  // sidebar actions
   if (action.type === SIDEBAR_OPEN) {
     return {...state, isSidebarOpen: true}
   }
@@ -17,6 +18,7 @@ const products_reducer = (state, action) => {
     return {...state, isSidebarOpen: false}
   }
 
+  // fetching products
   if (action.type === GET_PRODUCTS_BEGIN) {
     return {...state, products_loading: true}
   }
@@ -26,6 +28,15 @@ const products_reducer = (state, action) => {
   }
   if (action.type === GET_PRODUCTS_ERROR) {
     return {...state, products_loading: false, products_error: true}
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {...state, single_product_loading: true, single_product_error: false}
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {...state, single_product_loading: false, single_product: action.payload}
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {...state, single_product_loading: false, single_product_error: true}
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
