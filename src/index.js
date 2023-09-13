3c0f79dd41df15ef3b205f61cadc2abf58de487b
Behzad Aslani <behzadaslani80@gmail.com>
2023-09-13 13:22:23 +0330

    fix: removing stripe from the project
    it is not suppurted in my country

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// dev-4qamel3bp2w0hrvt.us.auth0.com
-// uukUDulUyLXmI7wThih2JXIpaazHj2vI
 root.render(
     <Auth0Provider
         domain={process.env.REACT_APP_AUTH_DOMAIN}
